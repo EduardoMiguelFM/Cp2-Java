f8803beb28d63c208870b7119ffc449591d940de
edumiguel <dudumfmonteiro@gmail.com>
2025-04-08 22:49:13 -0300

    Começo do projeto

---
+++ src/main/java/br/com/fiap/api_cp2/ApiCp2Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_cp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/api_cp2/ApiCp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_cp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
