ff945d203e52143b8b43827e80e63db8e891cf9f
edumiguel <dudumfmonteiro@gmail.com>
2025-04-11 06:50:05 -0300

    Projeto finalizado

--- src/main/java/br/com/fiap/api_cp2/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/api_cp2/config/SwaggerConfig.java
@@ -1,13 +1,28 @@
 package br.com.fiap.api_cp2.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.*;
+import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info().title("API Mottu").version("1.0"));
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Gestão de Entregadores - Mottu")
+                        .description("Esta API simula a operação da Mottu para controle de entregadores e motos alugadas. Projeto realizado pensando no Challengeadd ..")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Eduardo Miguel Forato Monteiro e Cicero Gabriel Oliveira Serafim")
+                        )
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT"))
+                );
     }
 }
